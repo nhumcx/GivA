0c506bc6be4b4a4bae62102e9210ad990f8e7a46
Eunjeong Park <ej.meyerpark@googlemail.com>
2022-09-06 12:49:51 +0200

    category dropdown

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,14 +1,11 @@
 class LocationsController < ApplicationController
   def index
-    # @locations = policy_scope(Location)
-
     if params[:query].present?
-      @locations = Location.where("address LIKE ?", "%#{params[:query]}%")
+      @locations = Location.where("category LIKE ?", "%#{params[:query]}%")
     else
       @locations = Location.all
     end
 
-    # The `geocoded` scope filters only locations with coordinates
     @markers = @locations.geocoded.map do |location|
       {
         lat: location.latitude,
